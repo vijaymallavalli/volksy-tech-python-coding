e6806580c77b247db7035515b396e5399a2a94be
vijaymallavalli <vijaysaradhim@brightcomgroup.com>
2022-12-27 09:10:51 +0000

    tasl

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-import random
-number = random.randit(-10000, 10000)
-n = number % 10
-if number < 0:
-    n = n - 10
-if n == 0:
-    print('last digit of',number,'is',n,'and is 0')
-else:
-    if n > 5:
-         print('last digit of',number,'is',n,'and is greater than 5'
-    else:
-        print('last digit of',number,'is',n,'and is less than 6 and not 0')      
+import random                                                                                                                           
+number = random.randint(-10000, 10000)                                                                                                  
+last = number % 10                                                                                                                      
+if number < 0:                                                                                                                          
+    last = last - 10                                                                                                                    
+if last == 0:                                                                                                                           
+    line = ('Last digit of ' + str(number) +                                                                                            
+            ' is ' + str(last) + ' and is 0')                                                                                           
+else:                                                                                                                                   
+    if last > 5:                                                                                                                        
+        line = ('Last digit of ' + str(number) +                                                                                        
+                ' is ' + str(last) + ' and is greater' +                                                                                
+                ' than 5')                                                                                                              
+    elif last < 5:                                                                                                                      
+        line = ('Last digit of ' + str(number) +                                                                                        
+                ' is ' + str(last) + ' and is less' +                                                                                   
+                ' than 6 and not 0')                                                                                                    
+print(line)                
+
+      
