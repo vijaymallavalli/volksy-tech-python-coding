8226c202f49d670281c565a3fd875ec8034a7487
vijaymallavalli <vijaysaradhim@brightcomgroup.com>
2022-12-27 09:26:43 +0000

    tasl

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random                                   
 number = random.randint(-10000, 10000)                                                                                                  
-last = number%10
-if number < 0 
+last = number % 10
+if number < 0: 
     last = last - 10
- last == 0:                                                                                                                           
+if  last == 0:                                                                                                                           
     line = ('Last digit of ' + str(number) +                                                                                            
             ' is ' + str(last) + ' and is 0')                                                                                           
 else:
