4f055e538bdd1eafa6f0b9188724334fecc89d8f
vijaymallavalli <vijaysaradhim@brightcomgroup.com>
2022-12-27 12:41:43 +0000

    new

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10)
-    for j in range(10)
-        if i == 0 and j == 9:
+for i in range(10):
+    for j in range(10):
+        if i == 8 and j == 9:
             print("89")
         elif i < j:
-            print("{:d}{:d}" .format(i, j), end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")
